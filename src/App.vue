62708f9e4449c4a62a45bd06efa1177acacc6cb9
Harry <harisathya1995@gmail.com>
2021-09-14 14:43:53 +0530

    stripeTest

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Stripe />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Stripe from './components/Stripe.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Stripe,
   },
 })
 export default class App extends Vue {}
---
+++ src/components/Stripe.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="root">
+    <h2>Stripe Integration</h2>
+    <button>Pay 500</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Stripe',
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  // LifeCycle Hook
+  created() {},
+};
+</script>
+
+<style></style>
