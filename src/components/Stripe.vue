d44541442b57980d9cb5fb921f53bb471f9245d7
Harry <harisathya1995@gmail.com>
2021-09-15 14:47:53 +0530

    addedCheckOutSession

--- src/components/Stripe.vue
+++ src/components/Stripe.vue
@@ -1,17 +1,42 @@
 <template>
   <div id="root">
     <h2>Stripe Integration</h2>
-    <button>Pay 500</button>
+    <h2>Create Customer</h2>
+    <a href="http://localhost:4500/createCustomer">Create Customer</a> <br />
+    <br />
+    <button @click="getCheckOutURL">Proceed</button>
+    <p>loading: {{ loading }}</p>
+    <a v-if="checkOutState" :href="checkoutURL">Checkout</a>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'Stripe',
   data() {
-    return {};
+    return {
+      loading: false,
+      checkOutState: false,
+      checkoutURL: '',
+    };
   },
-  methods: {},
+  methods: {
+    async getCheckOutURL() {
+      try {
+        this.loading = true;
+        let {
+          data: { data },
+        } = await axios.get('http://localhost:4500/createSession');
+        console.log(data);
+        this.loading = false;
+        this.checkoutURL = data.url;
+        this.checkOutState = true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
   computed: {},
   // LifeCycle Hook
   created() {},
